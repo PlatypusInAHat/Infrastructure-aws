c6df093da75a2910a41bf982ddac7e3b4a8f4c03
PlatypusInAHat <binh.nc225695@sis.hust.edu.vn>
2026-03-11 16:16:46 +0700

    chore: change eks node instance type to t3.small for free tier

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -50,7 +50,7 @@ variable "enabled_log_types" {
 variable "node_instance_types" {
   description = "Instance types for the managed node group"
   type        = list(string)
-  default     = ["t3.medium"]
+  default     = ["t3.small"]
 }
 
 variable "node_capacity_type" {
