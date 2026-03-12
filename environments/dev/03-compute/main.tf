8e42b9c1dbf86aec665ce49277a331e59838ae18
PlatypusInAHat <binh.nc225695@sis.hust.edu.vn>
2026-03-12 14:39:41 +0700

    refactor: integrate lb controller into terraform and clean up hardcoded manifests

--- environments/dev/03-compute/main.tf
+++ environments/dev/03-compute/main.tf
@@ -58,3 +58,50 @@ module "eks" {
   app_service_account_name  = var.app_service_account_name
   common_tags               = local.common_tags
 }
+
+# ---------- AWS Load Balancer Controller ----------
+
+resource "kubernetes_service_account" "lb_controller" {
+  metadata {
+    name      = "aws-load-balancer-controller"
+    namespace = "kube-system"
+    annotations = {
+      "eks.amazonaws.com/role-arn" = module.eks.lb_controller_role_arn
+    }
+  }
+}
+
+resource "helm_release" "lb_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  version    = "1.7.1"
+
+  set {
+    name  = "clusterName"
+    value = module.eks.cluster_name
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = kubernetes_service_account.lb_controller.metadata[0].name
+  }
+
+  set {
+    name  = "region"
+    value = var.region
+  }
+
+  set {
+    name  = "vpcId"
+    value = data.terraform_remote_state.network.outputs.vpc_id
+  }
+
+  depends_on = [module.eks]
+}
--- environments/dev/03-compute/providers.tf
+++ environments/dev/03-compute/providers.tf
@@ -14,6 +14,14 @@ terraform {
       source  = "hashicorp/tls"
       version = "~> 4.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
   }
 
   backend "s3" {
@@ -34,6 +42,30 @@ provider "aws" {
       Environment = var.environment
       ManagedBy   = "terraform"
       Layer       = "03-compute"
+    }
+  }
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
     }
   }
 }
